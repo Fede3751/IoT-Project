52cb871c04e41308088fe642605025e3ccee22f4
Fede3751 <ftrombetti@hotmail.it>
2023-04-26 16:17:37 +0200

    Fixed launch file to have the proper nodes to spawn

--- src/iot_project_solution/launch/launch.py
+++ src/iot_project_solution/launch/launch.py
@@ -7,6 +7,8 @@ def generate_launch_description():
   
   return LaunchDescription([
 
+    # Spawn a controller for each of the drone, in the
+    # correct namespace
     Node(
       package='iot_project_solution',
       executable='drone_controller',
@@ -14,15 +16,7 @@ def generate_launch_description():
       output='screen',
       emulate_tty=True,
       name='iot'
-    ),  
-    Node(
-      package='iot_project_solution',
-      executable='task_assigner',
-      namespace="X3_0",
-      output='screen',
-      emulate_tty=True,
-      name='iot'
-    ),  
+    ),
     Node(
       package='iot_project_solution',
       executable='drone_controller',
@@ -30,28 +24,22 @@ def generate_launch_description():
       output='screen',
       emulate_tty=True,
       name='iot'
-    ),  
+    ),
     Node(
       package='iot_project_solution',
-      executable='task_assigner',
-      namespace="X3_1",
+      executable='drone_controller',
+      namespace="X3_2",
       output='screen',
       emulate_tty=True,
+      name='iot'
+    ),
 
-      name='iot'
-    ),
-    Node(
-      package='iot_project_solution',
-      executable='drone_controller',
-      namespace="X3_2",
-      output='screen',
-      emulate_tty=True,
-      name='iot'
-    ),
+    # Spawn a task assigner outside the three namespaces.
+    # The task assigner should be able to communicate with
+    # all the three drones and submit the correct goals to achieve
     Node(
       package='iot_project_solution',
       executable='task_assigner',
-      namespace="X3_2",
       output='screen',
       emulate_tty=True,
       name='iot'
